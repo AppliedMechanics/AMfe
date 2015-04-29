037cde693cb62bda00a1051fd7ee3499b70813f3
Fabian Gruber <fabian.gruber@tum.de>
2015-04-29 12:28:35 +0000

    Minor changes ImportMesh.py

--- 002_Python/ImportMesh.py
+++ 002_Python/ImportMesh.py
@@ -11,24 +11,35 @@ Created on 20.04.2015
 '''
 
 import os
-import sys
+import sys 
 
-class Mesh:
+class Mesh():
+    """
+    Klasse Netz, in welcher die einzelnen import-Funktionen gesammelt werden
     
-    tag_format_start   = "$MeshFormat"
-    tag_format_end     = "$EndMeshFormat"
-    tag_nodes_start    = "$Nodes"
-    tag_nodes_end      = "$EndNodes"
-    tag_elements_start = "$Elements"
-    tag_elements_end   = "$EndElements"
+    Methods:
+        import_msh(filepath): import files of type .msh
+    
+    Attributes:
+    
+    """
  
     def __init__(self):
 
-        self.list_nodes = []
-        self.list_elements = []
-        self.list_properties = []
+        self.nodes = []
+        self.elements = []
+        self.properties = []
+   
         
     def import_msh(self, filepath):
+        tag_format_start   = "$MeshFormat"
+        tag_format_end     = "$EndMeshFormat"
+        tag_nodes_start    = "$Nodes"
+        tag_nodes_end      = "$EndNodes"
+        tag_elements_start = "$Elements"
+        tag_elements_end   = "$EndElements"
+        
+        
         try:
             infile = open(filepath,  'r')
         except:
@@ -38,19 +49,19 @@ class Mesh:
         infile.close()
         
         for s in data_geometry: # Auslesen der Indizes, bei denen die Formatliste, die Knotenliste und die Elementliste beginnen und enden
-            if s == type(self).tag_format_start: # Start Formatliste
+            if s == tag_format_start: # Start Formatliste
                 i_format_start   = data_geometry.index(s) + 1
-            elif s == type(self).tag_format_end: # Ende Formatliste
+            elif s == tag_format_end: # Ende Formatliste
                 i_format_end     = data_geometry.index(s)
-            elif s == type(self).tag_nodes_start: # Start Knotenliste
+            elif s == tag_nodes_start: # Start Knotenliste
                 i_nodes_start    = data_geometry.index(s) + 2
                 n_nodes          = int(data_geometry[i_nodes_start-1])
-            elif s == type(self).tag_nodes_end: # Ende Knotenliste
+            elif s == tag_nodes_end: # Ende Knotenliste
                 i_nodes_end      = data_geometry.index(s) 
-            elif s == type(self).tag_elements_start: # Start Elementliste 
+            elif s == tag_elements_start: # Start Elementliste 
                 i_elements_start = data_geometry.index(s) + 2
                 n_elements       = int(data_geometry[i_elements_start-1])
-            elif s == type(self).tag_elements_end: # Ende Elementliste 
+            elif s == tag_elements_end: # Ende Elementliste 
                 i_elements_end   = data_geometry.index(s)    
 
         
@@ -71,33 +82,25 @@ class Mesh:
         for j in range(len(list_imported_elements)):
             list_imported_elements[j] = [int(x) for x in list_imported_elements[j].split()] 
            
-        # Zeile [i] von [list_nodes] beinhaltet die X-, Y-, Z-Koordinate von Knoten [i+1]
-        self.list_nodes = [list_imported_nodes[j][1:] for j in range(len(list_imported_nodes))]
+        # Zeile [i] von [nodes] beinhaltet die X-, Y-, Z-Koordinate von Knoten [i+1]
+        self.nodes = [list_imported_nodes[j][1:] for j in range(len(list_imported_nodes))]
 
-        # Zeile [i] von [list_elements] beinhaltet die Knotennummern von Element [i+1]
+        # Zeile [i] von [elements] beinhaltet die Knotennummern von Element [i+1]
         for j in range(len(list_imported_elements)):
             # Nur fuer Dreieckselemente!!!
             if list_imported_elements[j][1] == 2:
                 tag = list_imported_elements[j][2]
-                self.list_properties.append(list_imported_elements[j][3:3+tag])
-                self.list_elements.append(list_imported_elements[j][3+tag:])              
+                self.properties.append(list_imported_elements[j][3:3+tag])
+                self.elements.append(list_imported_elements[j][3+tag:])              
 
 
-
-#data_folder = os.path.dirname(os.path.abspath(__file__)) + "/gmsh/" 
-#filename      = '2D_Rectangle_partition2.msh'  
-#filepath = data_folder + filename 
-#my_mesh = Mesh()
-#my_mesh.import_msh(filepath)
-#my_second_mesh = Mesh()
-#my_second_mesh.tag_format_start
 
 
 if __name__ == "__main__":
-    data_folder = os.path.dirname(os.path.abspath(__file__)) + "/gmsh/" 
-    filename      = '2D_Rectangle_partition2.msh'  
-    filepath = data_folder + filename 
-
+    subfolder = "/gmsh"
+    filename = "/2D_Rectangle_partition2.msh"
+    filepath = os.path.dirname(os.path.abspath(__file__)) + subfolder + filename
+    
     my_mesh = Mesh()
     my_mesh.import_msh(filepath)
 
