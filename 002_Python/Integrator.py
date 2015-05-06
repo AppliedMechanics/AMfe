9190b0501d46eee53f6a7c36219a20d5a1010570
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2015-05-06 18:55:20 +0000

    Integrator hinzugefügt

--- 002_Python/Integrator.py
+++ 002_Python/Integrator.py
@@ -44,8 +44,8 @@ class NewmarkIntegrator():
         Funktion, die das System nach der generalized-alpha-Methode integriert
         '''
         # Initialisieren der Startvariablen
-        q = q_start
-        dq = dq_start
+        q = q_start.copy()
+        dq = dq_start.copy()
         ddq = np.zeros(len(q_start))
         
         q_global = []
@@ -94,7 +94,7 @@ class NewmarkIntegrator():
                 res = self.residual(q, dq, ddq, t)
                 n_iter += 1
                 pass
-            
+
             # Writing if necessary:
             if write_flag:
                 q_global.append(q.copy())
@@ -107,45 +107,46 @@ class NewmarkIntegrator():
         return np.array(q_global), np.array(dq_global)
     
 
+#%%
 
-
-c1 = 1
-c2 = 2
-c3 = 1
-c4 = 0
-K = np.array([[c1 + c2,-c2,0], 
-              [-c2 , c2 + c3, -c3], 
-              [0, -c3, c3 + c4]])
+if __name__ == "__main__":
+    c1 = 10
+    c2 = 20
+    c3 = 10
+    c4 = 0
+    K = np.array([[c1 + c2,-c2,0], 
+                  [-c2 , c2 + c3, -c3], 
+                  [0, -c3, c3 + c4]])
+        
+    def my_k(q):
+        return K
     
-def my_k(q):
-    return K
-
-
-M = np.diag([3,1,2])
-
-def f_non(q):
-    return K.dot(q) + np.array([c1*q[0]**3, 0, 0])
-
-omega = 2*np.pi*3
-amplitude = 100
-def f_ext(q, dq, t):
-    return np.array([0, 0, amplitude*np.cos(omega*t)])
-
-
-
-
-q_start = np.array([1, 0, 2.])
-dq_start = q_start*0
-
-T = np.arange(0,100,0.01)
-
-my_integrator = NewmarkIntegrator()
-my_integrator.set_nonlinear_model(f_non, my_k, M)
-#my_integrator.verbose = True
-q, dq = my_integrator.integrate(q_start, dq_start, T)
-
-pyplot.plot(T, q)
-pyplot.plot(T, dq)
+    
+    M = np.diag([3,1,2])
+    
+    def f_non(q):
+        return K.dot(q) # + np.array([c1*q[0]**3, 0, 0])
+    
+    omega = 2*np.pi*1
+    amplitude = 5
+    def f_ext(q, dq, t):
+        return np.array([0, 0., amplitude*np.cos(omega*t)])
+    
+    
+    
+    
+    q_start = np.array([1, 0, 2.])*0
+    dq_start = q_start*0
+    
+    T = np.arange(0,30,0.01)
+    
+    my_integrator = NewmarkIntegrator()
+    my_integrator.set_nonlinear_model(f_non, my_k, M, f_ext)
+    #my_integrator.verbose = True
+    q, dq = my_integrator.integrate(q_start, dq_start, T)
+    
+    pyplot.plot(T, q)
+    #pyplot.plot(T, dq)
 
 
  #%%
