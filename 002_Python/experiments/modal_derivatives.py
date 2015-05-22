4f5924ee3faed1eb690bd0db553fb245dda5a614
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2015-05-22 15:56:08 +0000

    update

--- 002_Python/experiments/modal_derivatives.py
+++ 002_Python/experiments/modal_derivatives.py
@@ -20,8 +20,6 @@ import amfe
 gmsh_input_file = '../meshes/gmsh/bogen_grob.msh'
 paraview_output_file = '../results/gmsh_bogen_grob' + time.strftime("_%Y%m%d_%H%M%S") + '/bogen_grob'
 
-
-
 my_system = amfe.MechanicalSystem()
 my_system.load_mesh_from_gmsh(gmsh_input_file)
 # my_system.export_paraview(paraview_output_file)
@@ -45,15 +43,14 @@ my_dirichlet_bounds = [bottom_bounds_1, bottom_bounds_2]
 
 my_system.apply_dirichlet_boundaries(my_dirichlet_bounds)
 
-neumann_bounds = [[[amfe.node2total(i,1) for i in other_side], 'static', (3E6, ), None],
-                    [[amfe.node2total(i,0) for i in other_side], 'static', (1E6, ), None]]
+neumann_bounds = [[[amfe.node2total(i,1) for i in other_side], 'harmonic', (3E6, 100), None],
+                    [[amfe.node2total(i,0) for i in other_side], 'harmonic', (1E6, 200), None]]
 my_system.apply_neumann_boundaries(neumann_bounds)
 
-amfe.solve_linear_displacement(my_system)
 
 
+# amfe.solve_linear_displacement(my_system)
 
-#%%
 
 ndof = my_system.ndof_global_constrained
 K = my_system.K_global()
@@ -63,10 +60,22 @@ M = my_system.M_global()
 lambda_, V = sp.linalg.eigh(K.toarray(), M.toarray())
 omega = np.sqrt(lambda_)
 
-plt.semilogy(omega)
+# time integration
+my_newmark = amfe.NewmarkIntegrator()
+my_newmark.set_mechanical_system(my_system)
+my_newmark.delta_t = 5E-5
+my_newmark.integrate_nonlinear_system(np.zeros(ndof), np.zeros(ndof), np.arange(0,0.1,0.001))
 
-for i in range(40):
-    my_system.write_timestep(omega[i], V[:,i])
+# modal derivatives:
+
+
+
+
+## plotting results
+#plt.semilogy(omega)
+#
+#for i in range(40):
+#    my_system.write_timestep(omega[i], V[:,i])
 
 
 
