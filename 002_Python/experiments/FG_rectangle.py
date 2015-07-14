3fc1bb369e3c905c3ac655fb7d04ed98029f813f
Fabian Gruber <fabian.gruber@tum.de>
2015-07-14 15:04:43 +0000

    Beispiel Rechteck Eigenwertberechnung komplett lauffähig

--- 002_Python/experiments/FG_rectangle.py
+++ 002_Python/experiments/FG_rectangle.py
@@ -6,29 +6,28 @@ Created on Wed Jun 10 14:53:18 2015
 @author: gruber
 """
 
+
 import numpy as np
 import scipy as sp
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 import matplotlib.pyplot as plt
-import time
+
 
 # make amfe running
 import sys
-sys.path.insert(0,'..')
+sys.path.insert(0, '..')
 import amfe
 
 
 #%% Mesh generation
-x_len,y_len, x_no_elements, y_no_elements = 1,1,10,10 
-pos_x0, pos_y0 = 0,0 
-my_mesh_generator = amfe.MeshGenerator(x_len=x_len, y_len=y_len, 
-                                       x_no_elements=x_no_elements, 
-                                       y_no_elements=y_no_elements, 
-                                       pos_x0 = pos_x0, pos_y0 = pos_y0,
-                                       mesh_style = 'Quad4')
+x_len, y_len, x_no_elements, y_no_elements = 1, 1, 10, 10
+pos_x0, pos_y0 = 0, 0
+my_mesh_generator = amfe.MeshGenerator(x_len=x_len, y_len=y_len,
+                                       x_no_elements=x_no_elements,
+                                       y_no_elements=y_no_elements,
+                                       pos_x0=pos_x0, pos_y0=pos_y0,
+                                       mesh_style='Quad4')
 my_mesh_generator.build_mesh()
-my_mesh_generator.save_mesh('..meshes/selbstgebaut_quad/nodes.csv', 
+my_mesh_generator.save_mesh('../meshes/selbstgebaut_quad/nodes.csv',
                             '../meshes/selbstgebaut_quad/elements.csv')
 
 
@@ -38,103 +37,123 @@ my_mesh_generator.save_mesh('..meshes/selbstgebaut_quad/nodes.csv',
 my_system = amfe.MechanicalSystem()
 
 # Load mesh
-my_system.load_mesh_from_csv('../meshes/selbstgebaut_quad/nodes.csv', 
+my_system.load_mesh_from_csv('../meshes/selbstgebaut_quad/nodes.csv',
                              '../meshes/selbstgebaut_quad/elements.csv')
 
 
 #dirichlet_boundary conditions
 #top_fixation_2 = [None, [master_node - 1 + 2*x for x in range(11)], None]
-nodes_to_fix = np.where(my_system.node_list[:,0] == pos_x0)[0]
-dofs_to_fix = np.concatenate((nodes_to_fix*2,nodes_to_fix*2+1),axis=1)
+nodes_to_fix = np.where(my_system.node_list[:, 0] == pos_x0)[0]
+dofs_to_fix = np.concatenate((nodes_to_fix*2, nodes_to_fix*2+1), axis=1)
 fixation_left = [None, dofs_to_fix, None]
 dirichlet_boundary_list = [fixation_left]
-my_system.apply_dirichlet_boundaries(dirichlet_boundary_list)    
-       
-M, K = amfe.give_mass_and_stiffness(my_system)  
+my_system.apply_dirichlet_boundaries(dirichlet_boundary_list)
+
+# Build mass and stiffness matrix
+M, K = amfe.give_mass_and_stiffness(my_system)
 
 
-
-
-
-
-
-
-
+#%% Plot mesh
 # Copy lists for plotting
 node_list = np.copy(my_system.node_list)
 element_list = np.copy(my_system.element_list)
+pos_of_nodes = node_list.reshape((-1, 1))
 
 
-
-# Plot nodes
-plt.figure(5)
-print('Plotting nodes',end='')
-plt.plot(node_list[:,0],node_list[:,1],'gx')
-for i_nod in range(len(node_list)):
-    plt.text(node_list[i_nod,0],node_list[i_nod,1],i_nod,
-             verticalalignment='bottom',color='g')
-plt.axis('equal')
-#plt.axis([pos_x0-0.1, pos_x0+x_len+0.1, pos_y0-0.1, pos_y0+y_len+0.1])
-plt.title('Nodes of the mesh')
-print(' successful!')
-
-
-
-
-# Create a list of elements that every row is one closed loop of nodes
-ele_list_plot = np.concatenate((element_list,
-                                element_list[:,0].reshape(len(element_list),1)),
-                                axis=1)
-                                
-plt.figure(6)
-print('Plotting elements',end='')
-plot_no_of_ele = True
-for i_ele in range(len(ele_list_plot)):
-    nodes = ele_list_plot[i_ele,:]
-    plt.plot(node_list[nodes,0], node_list[nodes,1],color='b')
-    if plot_no_of_ele:
-        coor = 0.5*node_list[nodes[0],:] + 0.5*node_list[nodes[2],:]
-        plt.text(coor[0],coor[1],i_ele,color='b',horizontalalignment='center',
-                 verticalalignment='center')
-plt.axis('equal')
-#plt.axis([pos_x0-0.1, pos_x0+x_len+0.1, pos_y0-0.1, pos_y0+y_len+0.1])
-plt.title('Mesh')
-print(' successful!')
+def plot_nodes_Quad4(coord, p_col='b', no_of_fig=1,
+                     p_title='Nodes of the mesh'):
+    print('Plotting nodes...', end='')
+    plt.figure(no_of_fig)
+    plt.plot(coord[0::2], coord[1::2], color=p_col, marker='o', linestyle='')
+    for i_nod in range(int(len(coord)/2)):
+        plt.text(coord[2*i_nod], coord[2*i_nod+1], i_nod,
+                 verticalalignment='bottom', color=p_col)
+    plt.axis('equal')
+    #plt.axis([pos_x0-0.1, pos_x0+x_len+0.1, pos_y0-0.1, pos_y0+y_len+0.1])
+    plt.title('Nodes of the mesh')
+    plt.hold(True)
+    print(' successful!')
 
 
+def plot_mesh_Quad4(elements, coord, plot_no_of_ele=False, plot_nodes=False,
+                    p_col='r', no_of_fig=1, p_title='Mesh'):
+
+    '''
+    Plot mesh of plane Quad4 elements
+
+    Parameters
+    ----------
+    elements: array
+        array containing the node number of each element
+    coord: array
+        array containing the coordinates of each node
+    plot_no_of_ele: bool, optional
+        flag stating if numbers of elements are plotted
+    plot_nodes: bool, optional
+        flag stating if nodes are plotted
+    p_col: str, optional
+        color of plot
+
+    Returns
+    -------
+    None
+    '''
+    print('Plotting elements...', end='')
+    plt.figure(no_of_fig)
+
+    # Duplicate first node of each element to get closed circle
+    ele_list_plot = np.concatenate((element_list,
+                                    element_list[:, 0]
+                                    .reshape(len(element_list), 1)), axis=1)
+
+    # Loop over elements
+    for i_ele in range(len(elements)):
+
+        nodes = ele_list_plot[i_ele, :]  # Node number of one element
+        plt.plot(coord[nodes*2], coord[nodes*2+1], color=p_col)
+
+        # Plot number of element in each element if plot_no_of_ele = True
+        if plot_no_of_ele:
+            dof1 = nodes[0]*2+np.array([0, 1])
+            dof2 = nodes[2]*2+np.array([0, 1])
+            pos_of_text = 0.5*coord[dof1] + 0.5*coord[dof2]
+            plt.text(pos_of_text[0], pos_of_text[1], i_ele, color=p_col,
+                     horizontalalignment='center', verticalalignment='center')
+
+    if plot_nodes:
+        plot_nodes_Quad4(coord, no_of_fig=no_of_fig, p_title=p_title)
+
+    # Plot properties
+    plt.axis('equal')
+    plt.title(p_title)
+    plt.hold(True)
+    print(' successful!')
+
+
+# Plot mesh and nodes of Quad4-elements
+plot_nodes_Quad4(pos_of_nodes, p_col='b', no_of_fig=1)
+plot_mesh_Quad4(element_list, pos_of_nodes, plot_no_of_ele=True, p_col='b',
+                no_of_fig=2, p_title='Reine Vernetzung')
 
 
 #%% Compute eigenvalues
-lam, phi = sp.sparse.linalg.eigsh(K,k=6,M=M,which='SM')
+lam, phi = sp.sparse.linalg.eigsh(K, k=20, M=M, which='SM')
 
-no_of_eigenm = 5
-scale = -0.05
 
+# Extend computed eigenmodes to all dofs
 disp_glob = my_system.b_constraints * phi
-pos = node_list.reshape((-1,1)) + disp_glob*scale 
+# Add eigenmode displacement (multiplied by scaling factor) to positions node
+scale = -0.05
+pos = pos_of_nodes + disp_glob*scale
 
 
-t1 = time.time()
-plt.figure(7)
-print('Plotting mode {0} (= eigenmodes number {1}, including rigid body modes)'
-      .format(no_of_eigenm,no_of_eigenm+1),end='')
-
-for i_ele in range(len(ele_list_plot)):
-    nodes = ele_list_plot[i_ele,:]
-    dof_x = nodes*2
-    dof_y = nodes*2+1
-    plt.plot(pos[dof_x,no_of_eigenm], pos[dof_y,no_of_eigenm],color='r')
-plt.axis('equal')    
-#plt.axis([pos_x0-0.1, pos_x0+x_len+0.1, pos_y0-0.1, pos_y0+y_len+0.1])
-plt.title('Mesh')
-print(' successful!')
-
-delta_t = time.time() - t1
-print('Benötigte Zeit: {0}'.format(delta_t))
-
-
-
+# Plot mesh of Quad4-elements
+no_of_eigenm = 10    # = true eigenmodes number is no_of_eigenmode + 1!
+p_title = 'Eigenmode {0} (including rigid body modes)'.format(no_of_eigenm+1)
+plot_mesh_Quad4(element_list, pos[:, no_of_eigenm], plot_no_of_ele=True,
+                plot_nodes=True, p_col='r', no_of_fig=7, p_title=p_title)
 
 plt.show()
-
-
+#t1 = time.time()
+#delta_t = time.time() - t1
+#print('Benötigte Zeit: {0}'.format(delta_t))
