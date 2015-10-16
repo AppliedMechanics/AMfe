becf1362c681649ca77917356a558e50042ba3c4
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2015-10-16 17:46:17 +0200

    Update in Fortran compilation script

--- f2py/install_fortran_routines.sh
+++ f2py/install_fortran_routines.sh
@@ -5,3 +5,5 @@ echo 'Install the following fortran routines into AMfe:\nAssembly\nElement'
 # Compile the fortran routines and install them into the amfe folder
 f2py3 -c  --fcompiler=gnu95 -m f90_assembly assembly.f90 && cp f90_assembly*.so ../amfe/
 f2py3 -c  --fcompiler=gnu95 -m f90_element element.f90 && cp f90_element*.so ../amfe/
+
+echo 'FORTRAN Compile successfully'
