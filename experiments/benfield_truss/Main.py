f186ac887e84f56183c185aa064240ab2d964315
Fabian Gruber <fabian.gruber@tum.de>
2015-11-12 19:25:24 +0100

    Benfield truss working.

--- experiments/benfield_truss/Main.py
+++ experiments/benfield_truss/Main.py
@@ -17,8 +17,8 @@ import amfe
 import plot_bar
 
 #%% Geometric parameters
-l1 = 4
-l2 = 3
+l1 = 4.0
+l2 = 3.0
 
 E_modul=1.0
 density=1.0
@@ -52,7 +52,7 @@ for i in range(49,65):
     elements = np.append(elements,elements[i-2,:]+[[3,3]],axis=0)
 
 
-my_mesh_generator = amfe.MeshGenerator()
+my_mesh_generator = amfe.MeshGenerator(mesh_style='Bar2D')
 my_mesh_generator.nodes = nodes
 my_mesh_generator.elements = elements
 my_mesh_generator.save_mesh('./mesh/full_system/nodes.csv',
@@ -62,18 +62,30 @@ my_mesh_generator.save_mesh('./mesh/full_system/nodes.csv',
 #%% Building the mechanical system
 
 # Initialize system
-my_system = amfe.MechanicalSystem()
-my_system = amfe.MechanicalSystem(E_modul=1.0, crosssec=1.0, density=1.0,
-                                  mesh_style='Bar')
+my_system = amfe.MechanicalSystem(E_modul=1.0, crosssec=1.0, density=1.0)
 # Load mesh
 my_system.load_mesh_from_csv('./mesh/full_system/nodes.csv',
-                             './mesh/full_system/elements.csv')
+                             './mesh/full_system/elements.csv',
+                             ele_type = 'Bar2Dlumped')
 
+
+# Dirichlet_boundary conditions
+my_system.apply_dirichlet_boundaries()
+
+
+# Build mass and stiffness matrix
+M, K = amfe.give_mass_and_stiffness(my_system)
+lam, phi_i = sp.sparse.linalg.eigsh(K, k=20, M=M, which='SM')
 
 # Plot mesh of bars
 pos_of_nodes = nodes.reshape((-1, 1))    
-plot_bar.plt_mesh(elements, pos_of_nodes, plot_no_of_ele=True, 
-                  plot_nodes=True, p_col='b', no_of_fig=1, p_title='Mesh',
+plot_bar.plt_mesh(elements, pos_of_nodes, plot_no_of_ele=False, 
+                  plot_nodes=False, p_col='0.25', no_of_fig=1, p_title='Mesh',
                   p_col_node='r')   
+scale = 20
+disp_eigenmodes = pos_of_nodes + phi_i*scale
+plot_bar.plt_mesh(elements, disp_eigenmodes[:,6], plot_no_of_ele=True, 
+                  plot_nodes=True, p_col='b', no_of_fig=1, p_title='Eigenmode',
+                  p_col_node='r')
 
 plt.show()
