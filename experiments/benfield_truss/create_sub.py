38f67d9c0220c3cd0f4308a0937e072baebb2251
Fabian Gruber <fabian.gruber@tum.de>
2015-11-16 18:29:44 +0100

    Add experiments/benfield_truss/create_sub to create the substructures in own module.

---
+++ experiments/benfield_truss/create_sub.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 17:57:24 2015
+
+@author: fabian
+"""
+
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+# make amfe running
+import sys
+sys.path.insert(0, '../..')
+import amfe
+import plot_bar
+
+
+
+def create_substructure():
+    #%% Geometric parameters
+    l1 = 4.0
+    l2 = 3.0
+    
+    E_modul=1.0
+    density=1.0
+    A = 1.0
+    
+    #%% Left substructure
+    # Nodes
+    nodes = np.array([[0, 0]])
+    nodes = np.append(nodes,[[0,l2]],axis=0)
+    nodes = np.append(nodes,[[0,2*l2]],axis=0)
+    for i in range(nodes.shape[0],18):  
+        nodes = np.append(nodes,nodes[i-3,:]+[[l1,0]],axis=0)
+        
+    # Elements 
+    # Vertical bars
+    elements = np.array([[0,1]])
+    elements = np.append(elements,[[1,2]],axis=0)
+    for i in range(2,12): #12 statt 20
+        elements = np.append(elements,elements[i-2,:]+[[3,3]],axis=0) 
+        
+    # Horizontal bars
+    elements = np.append(elements,[[0,3]],axis=0)
+    elements = np.append(elements,[[1,4]],axis=0)
+    elements = np.append(elements,[[2,5]],axis=0)
+    for i in range(15,27): 
+        elements = np.append(elements,elements[i-3,:]+[[3,3]],axis=0)    
+    
+    # Diagonal bars
+    elements = np.append(elements,[[3,1]],axis=0)
+    elements = np.append(elements,[[1,5]],axis=0)
+    for i in range(29,37): 
+        elements = np.append(elements,elements[i-2,:]+[[3,3]],axis=0)
+     
+   
+    # Call mesh_generator to save the mesh as csv-files
+    my_mesh_generator = amfe.MeshGenerator(mesh_style='Bar2D')
+    my_mesh_generator.nodes = nodes
+    my_mesh_generator.elements = elements
+    my_mesh_generator.save_mesh('./mesh/substructures/nodes-sub1.csv',
+                                './mesh/substructures/elements-sub1.csv')
+                               
+           
+                                
+    #%% Right substructure
+                                
+    # Nodes
+    nodes = np.array([[l1*5, 0]])
+    nodes = np.append(nodes,[[l1*5,l2]],axis=0)
+    nodes = np.append(nodes,[[l1*5,2*l2]],axis=0)
+    for i in range(nodes.shape[0],15):  
+        nodes = np.append(nodes,nodes[i-3,:]+[[l1,0]],axis=0)                            
+                            
+    # Elements right substructure
+    # Vertical bars
+    elements = np.array([[3,4]])
+    elements = np.append(elements,[[4,5]],axis=0)
+    for i in range(2,8):
+        elements = np.append(elements,elements[i-2,:]+[[3,3]],axis=0) 
+        
+    # Horizontal bars
+    elements = np.append(elements,[[0,3]],axis=0)
+    elements = np.append(elements,[[1,4]],axis=0)
+    elements = np.append(elements,[[2,5]],axis=0)
+    for i in range(11,20):
+        elements = np.append(elements,elements[i-3,:]+[[3,3]],axis=0)    
+    
+    # Diagonal bars
+    elements = np.append(elements,[[3,1]],axis=0)
+    elements = np.append(elements,[[1,5]],axis=0)
+    for i in range(22,28):
+        elements = np.append(elements,elements[i-2,:]+[[3,3]],axis=0)
+            
+    # Call mesh_generator to save the mesh as csv-files
+    my_mesh_generator = amfe.MeshGenerator(mesh_style='Bar2D')
+    my_mesh_generator.nodes = nodes
+    my_mesh_generator.elements = elements
+    my_mesh_generator.save_mesh('./mesh/substructures/nodes-sub2.csv',
+                                './mesh/substructures/elements-sub2.csv')
+    
+    
