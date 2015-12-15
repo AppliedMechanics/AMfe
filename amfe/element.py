08a0eb38d5aa6d2ab9bb1f44ac5e307fdc8cf585
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2015-12-15 15:43:46 +0100

    Update on the skin elements

--- amfe/element.py
+++ amfe/element.py
@@ -1068,22 +1068,34 @@ class Tri6Boundary(BoundaryElement):
     necessary. 
     '''
     B0_dict = {}
-    B0_dict.update({'normal' : np.vstack((np.eye(3), np.eye(3), np.eye(3)))/3})
-    B0 = np.zeros((9,3))
-    B0[np.ix_([0,3,6], [0])] = 1/3
+    B0_dict.update({'normal' : np.vstack((np.zeros(3*3,3),np.eye(3), np.eye(3), np.eye(3)))/3})
+    B0 = np.zeros((18,3))
+    B0[np.ix_([9,12,15], [0])] = 1/3
     B0_dict.update({'x_n' : B0})
-    B0 = np.zeros((9,3))
-    B0[np.ix_([1,4,7], [1])] = 1/3
+    B0 = np.zeros((18,3))
+    B0[np.ix_([10,13,16], [0])] = 1/3
     B0_dict.update({'y_n' : B0})
-    B0 = np.zeros((9,3))
-    B0[np.ix_([2,5,8], [2])] = 1/3
+    B0 = np.zeros((18,3))
+    B0[np.ix_([11,14,17], [0])] = 1/3
     B0_dict.update({'z_n' : B0})
-    B0_dict.update({'x' : np.array([1/3, 0, 0, 1/3, 0, 0, 1/3, 0, 0])})
-    B0_dict.update({'y' : np.array([0, 1/3, 0, 0, 1/3, 0, 0, 1/3, 0])})
-    B0_dict.update({'z' : np.array([0, 0, 1/3, 0, 0, 1/3, 0, 0, 1/3])})
+    B0_dict.update({'x' : np.array([0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 
+                                    1/3, 0, 0, 1/3, 0, 0, 1/3, 0, 0])})
+    B0_dict.update({'y' : np.array([0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
+                                    0, 1/3, 0, 0, 1/3, 0, 0, 1/3, 0])})
+    B0_dict.update({'z' : np.array([0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
+                                    0, 0, 1/3, 0, 0, 1/3, 0, 0, 1/3])})
 
-    def __init__(self, val, direct, time_func=None):
+    gauss_points = ((1/6, 1/6, 2/3, 1/3),
+                     (1/6, 2/3, 1/6, 1/3),
+                     (2/3, 1/6, 1/6, 1/3))
+
+
+    def __init__(self, val, direct, time_func=None, full_integration=False):
         super().__init__(val, direct, time_func=time_func, ndof=18)
+        # ovlerloading of _compute_tensors function for case that full 
+        # integration is valid
+        if full_integration:
+            self._compute_tensors = self._compute_tensors_full
     
     def _compute_tensors(self, X, u, t):
         x_vec = (X+u).reshape((-1, 3)).T
@@ -1092,7 +1104,42 @@ class Tri6Boundary(BoundaryElement):
         n = np.cross(v1, v2)/2
         self.f = self.f_func(n) * self.val * self.time_func(t)
 
-        pass
+    def _compute_tensors_full(self, X, u, t):
+        '''
+        Compute the full pressure contribution by doing gauss integration. 
+        '''
+        X1, Y1, X2, Y2, X3, Y3, X4, Y4, X5, Y5, X6, Y6 = X
+        N = np.zeros(6)
+        # gauss point evaluation of full pressure field
+        for L1, L2, L3, w in self.gauss_points:
+            # the entries in the jacobian dX_dL
+            Jx1 = 4*L2*X4 + 4*L3*X6 + X1*(4*L1 - 1)
+            Jx2 = 4*L1*X4 + 4*L3*X5 + X2*(4*L2 - 1)
+            Jx3 = 4*L1*X6 + 4*L2*X5 + X3*(4*L3 - 1)
+            Jy1 = 4*L2*Y4 + 4*L3*Y6 + Y1*(4*L1 - 1)
+            Jy2 = 4*L1*Y4 + 4*L3*Y5 + Y2*(4*L2 - 1)
+            Jy3 = 4*L1*Y6 + 4*L2*Y5 + Y3*(4*L3 - 1)
+
+            det = Jx1*Jy2 - Jx1*Jy3 - Jx2*Jy1 + Jx2*Jy3 + Jx3*Jy1 - Jx3*Jy2
+            A = det/2
+
+            N_w = np.array([L1*(2*L1 - 1), L2*(2*L2 - 1), L3*(2*L3 - 1), 
+                          4*L1*L2, 4*L2*L3, 4*L1*L3])
+            N += N_w * A
+            
+        x_vec = (X+u).reshape((-1, 3)).T
+        v1 = x_vec[:,2] - x_vec[:,0]
+        v2 = x_vec[:,1] - x_vec[:,0]
+        n = np.cross(v1, v2)/2
+        # normalize:
+        n /= np.sqrt(n @ n)
+        f = self.f_func(n) * self.val * self.time_func(t)
+        # correct the contributions of the standard B-matrix with the one 
+        # computed with gauss integration
+        B_corr = 3 * np.array([(i, i, i) for i in N]).flatten()
+        self.f = B_corr*f
+        
+
 
 class LineLinearBoundary(BoundaryElement):
     '''
