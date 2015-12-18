98afb0ae0682b14b7e1482d803bed697caa7620b
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2015-12-18 11:43:18 +0100

    Update setup.py so that the package can be installed now using python setup.py develop

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 21 12:06:20 2015
-
-@author: johannesr
 """
 
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Finite Element Code for simple cases',
+    'name': 'amfe',
+    'version': '0.2',
+    'description': 'Nonlinear Finite Element Code with simplicity in mind',
     'author': 'Johannes Rutzmoser',
     'url': 'No URL provided yet',
     'download_url': 'Where to download it.',
     'author_email': 'johannes.rutzmoser@tum.de',
-    'version': '0.1',
-    'install_requires': ['nose'],
-    'packages': [],
+    'install_requires': ['numpy', 'scipy', 'pandas', 'sphinx>=1.3.0'],
+    'tests_require': ['nose'],
+    'packages': find_packages(where='amfe'),
     'scripts': [],
-    'name': 'amfe'
+    'entry_points': {},
 }
 
 setup(**config)
