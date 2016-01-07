bf98baf518f0757c5d7b5cf36beb18227cfc6ff7
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2016-01-07 12:47:52 +0100

    Update of wiggling u

--- examples/wiggling_u.py
+++ examples/wiggling_u.py
@@ -10,21 +10,17 @@ import numpy as np
 import scipy as sp
 import time
 
-from matplotlib import pyplot as plt
-# make amfe running
-import sys
-sys.path.insert(0, '..')
 import amfe
-
-
 
 gmsh_input_file = '../meshes/gmsh/bogen_grob.msh'
 paraview_output_file = '../results/gmsh_bogen_grob' + time.strftime("_%Y%m%d_%H%M%S") + '/bogen_grob'
+
 
 my_material = amfe.KirchhoffMaterial()
 my_system = amfe.MechanicalSystem()
 
 my_system.load_mesh_from_gmsh(gmsh_input_file, 15, my_material)
+# Test the paraview basic output 
 # my_system.export_paraview(paraview_output_file)
 
 my_system.apply_dirichlet_boundaries(13, 'xy')
@@ -32,8 +28,8 @@ my_system.apply_dirichlet_boundaries(13, 'xy')
 harmonic_x = lambda t: np.sin(2*np.pi*t*30)
 harmonic_y = lambda t: np.sin(2*np.pi*t*50)
 
-my_system.apply_neumann_boundaries(14, 6E6, 'x', harmonic_x)
-my_system.apply_neumann_boundaries(14, 6E6, 'y', harmonic_y)
+my_system.apply_neumann_boundaries(14, 6E7, 'x', harmonic_x)
+my_system.apply_neumann_boundaries(14, 6E7, 'y', harmonic_y)
 
 
 ###############################################################################
@@ -45,7 +41,12 @@ ndof = my_system.dirichlet_class.no_of_constrained_dofs
 my_newmark = amfe.NewmarkIntegrator()
 my_newmark.set_mechanical_system(my_system)
 my_newmark.delta_t = 1E-4
+t1 = time.time()
 my_newmark.integrate_nonlinear_system(np.zeros(ndof), np.zeros(ndof), np.arange(0,0.4,1E-4))
+t2 = time.time()
 
+print('Time for computation:', t2 - t1, 'seconds.')
 
 my_system.export_paraview(paraview_output_file)
+t3 = time.time()
+print('Time for export:', t3 - t2, 'seconds.')
