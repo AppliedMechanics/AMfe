4f7ff6197ae8123ad11f7a4810b4470a04f8f73a
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2016-05-18 18:25:44 +0200

    removed redundant stuff in example

--- examples/pressure_corner.py
+++ examples/pressure_corner.py
@@ -25,26 +25,6 @@ my_system.export_paraview(output_file)
 
 
 
-
-
-input_file = '../meshes/gmsh/pressure_corner.msh'
-output_file = '../results/pressure_corner/pressure_corner'
-
-
-my_material = amfe.KirchhoffMaterial(E=210E9, nu=0.3, rho=1E4, plane_stress=True)
-my_system = amfe.MechanicalSystem()
-my_system.load_mesh_from_gmsh(input_file, 11, my_material)
-my_system.apply_dirichlet_boundaries(9, 'x')
-my_system.apply_dirichlet_boundaries(10, 'y')
-my_system.apply_neumann_boundaries(12, 1E10, 'normal', lambda t: t)
-
-
-#amfe.solve_linear_displacement(my_system)
-amfe.solve_nonlinear_displacement(my_system, no_of_load_steps=50)
-
-my_system.export_paraview(output_file)
-
-
 #%% Modal analysis
 
 # omega, V = amfe.vibration_modes(my_system, save=True)
