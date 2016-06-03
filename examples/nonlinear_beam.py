d4cb88d6bccc4a4840b4bb866fb25b09770254e7
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2016-06-03 14:51:24 +0200

    WIP: Bar example for presentation

--- examples/nonlinear_beam.py
+++ examples/nonlinear_beam.py
@@ -8,7 +8,7 @@ import amfe
 
 
 input_file = '../meshes/gmsh/bar.msh'
-output_file = '../results/beam_nonlinear/beam_nolinear'
+output_file = '../results/beam_nonlinear/beam_nonlinear'
 
 
 my_material = amfe.KirchhoffMaterial(E=210E9, nu=0.3, rho=1E4, plane_stress=True)
@@ -19,10 +19,11 @@ my_system.apply_neumann_boundaries(key=9, val=1E8, direct=(0,-1),
                                    time_func=lambda t: t)
 
 
-amfe.solve_linear_displacement(my_system)
-#amfe.solve_nonlinear_displacement(my_system, no_of_load_steps=50)
+#amfe.solve_linear_displacement(my_system)
+amfe.solve_nonlinear_displacement(my_system, no_of_load_steps=50)
 
-my_system.export_paraview(output_file + '_linear')
+#my_system.export_paraview(output_file + '_linear')
+my_system.export_paraview(output_file)
 
 
 
