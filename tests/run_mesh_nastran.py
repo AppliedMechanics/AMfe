75479b482b00a055af2644d8639b27cf043fac6f
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2016-09-20 17:23:19 +0200

    modified some further tests

--- tests/run_mesh_nastran.py
+++ tests/run_mesh_nastran.py
@@ -15,6 +15,9 @@ my_mesh = amfe.Mesh()
 my_mesh.import_bdf(filename)
 
 my_mesh.load_group_to_mesh(1, my_material)
+my_mesh.load_group_to_mesh(10, my_material)
+my_mesh.load_group_to_mesh(20, my_material)
+
 #%%
 
 my_assembly = amfe.Assembly(my_mesh)
@@ -24,3 +27,7 @@ u0 = np.zeros(my_mesh.no_of_dofs)
 K_unconstr, f_unconstr = my_assembly.assemble_k_and_f(u=u0, t=1)
 
 my_mesh.save_mesh_xdmf('results/nastran_meshes/mesh_nastran')
+
+#%%
+
+K_reord, perm = amfe.tools.reorder_sparse_matrix(K_unconstr)
