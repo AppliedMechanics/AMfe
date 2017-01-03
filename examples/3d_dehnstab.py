bb832716397815593718082e06d7d2c81240bf3c
Christian Meyer <christian.meyer@tum.de>
2017-01-03 18:20:11 +0100

    Update examples/3d_dehnstab.py - changes for PEP8-conformity

--- examples/3d_dehnstab.py
+++ examples/3d_dehnstab.py
@@ -4,23 +4,17 @@ Running a 3D-tension bar
 """
 
 
-import sys
 import time
 
-import numpy as np
-import scipy as sp
 import amfe
 
-
-import matplotlib.pyplot as plt
 
 mesh_file = amfe.amfe_dir('meshes/test_meshes/bar_Tet4_fine.msh')
 
 # Building the mechanical system
 my_material = amfe.KirchhoffMaterial()
 my_mechanical_system = amfe.MechanicalSystem()
-my_mechanical_system.load_mesh_from_gmsh(mesh_file, 
-                                         29, my_material)
+my_mechanical_system.load_mesh_from_gmsh(mesh_file, 29, my_material)
 # Fixations are simple to realize
 my_mechanical_system.apply_dirichlet_boundaries(30, 'xyz')
 my_mechanical_system.apply_dirichlet_boundaries(31, 'yz')
@@ -30,18 +24,16 @@ nodes, dofs = my_mechanical_system.mesh_class.set_dirichlet_bc(31, 'x', output='
 my_mechanical_system.dirichlet_class.master_slave_list.append([dofs[0], dofs, None])
 my_mechanical_system.dirichlet_class.update()
 
-#%%
+# %%
 
 # static force in y-direction
 # my_neumann_boundary_list = [[[dofs[0],], 'static', (1E10, ), None]]
-my_mechanical_system.apply_neumann_boundaries(31, 1E12, (1,0,0), lambda t: t)
+my_mechanical_system.apply_neumann_boundaries(31, 1E12, (1, 0, 0), lambda t: t)
 
 
 # static solution
 amfe.solve_nonlinear_displacement(my_mechanical_system, 10)
-#amfe.solve_linear_displacement(my_mechanical_system)
-export_path = amfe.amfe_dir('results/bar_tet10' + 
+# amfe.solve_linear_displacement(my_mechanical_system)
+export_path = amfe.amfe_dir('results/bar_tet10' +
                             time.strftime("_%Y%m%d_%H%M%S") + '/bar_tet4')
 my_mechanical_system.export_paraview(export_path)
-
-
