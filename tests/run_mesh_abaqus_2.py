1467dbae7ff962a54857f20d370adc8e37aeb5af
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2017-01-27 16:39:46 +0100

    added abaqus run for leaf spring

--- tests/run_mesh_abaqus_2.py
+++ tests/run_mesh_abaqus_2.py
@@ -8,33 +8,40 @@ import amfe
 input_file = amfe.amfe_dir('meshes/abaqus/A9603205802.inp')
 output_file = amfe.amfe_dir('results/abaqus/leaf_spring_2')
 
-my_mesh = amfe.Mesh()
+my_system = amfe.MechanicalSystem()
+
+my_mesh = my_system.mesh_class
 my_mesh.import_inp(input_file)
 
+steel = amfe.KirchhoffMaterial(E=210E9, nu=0.3, rho=1E4, plane_stress=True)
 my_material = amfe.KirchhoffMaterial(E=210E9, nu=0.3, rho=1E4, plane_stress=True)
 
-#my_mesh.load_group_to_mesh('NX_3d_mesh(65)', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('Leaf_1', steel, 'phys_group')
+my_mesh.load_group_to_mesh('Leaf_2', steel, 'phys_group')
+
+my_mesh.load_group_to_mesh('NX_3d_mesh(65)', my_material, 'phys_group')
 my_mesh.load_group_to_mesh('Lager_Lower_Platte', my_material, 'phys_group')
 
 my_mesh.load_group_to_mesh('LAger_Upper_Platte', my_material, 'phys_group')
-my_mesh.load_group_to_mesh('Leaf_1', my_material, 'phys_group')
-my_mesh.load_group_to_mesh('Leaf_2', my_material, 'phys_group')
 my_mesh.load_group_to_mesh('Platte', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Leaf_1', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('P13', my_material, 'phys_group')
 
-
-#my_mesh.load_group_to_mesh('Federspannplate', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Zwischenplatte', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Blatt_01', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Blatt_02', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Blatt_03', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Blatt_04', my_material, 'phys_group')
-#my_mesh.load_group_to_mesh('Blatt_05', my_material, 'phys_group')
-
-my_mesh.save_mesh_xdmf(output_file + '_all')
+my_mesh.save_mesh_xdmf(output_file + '_solid')
 
 #%%
+my_mesh.load_group_to_mesh('', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('', my_material, 'phys_group')
+my_mesh.load_group_to_mesh('', my_material, 'phys_group')
 
-#my_system = amfe.MechanicalSystem(stress_recovery=True)
 
+#%%
+my_system.no_of_dofs_per_node = my_mesh.no_of_dofs_per_node
+my_system.assembly_class.preallocate_csr()
+my_system.dirichlet_class.no_of_unconstrained_dofs = my_mesh.no_of_dofs
+my_system.dirichlet_class.update()
 
+K = my_system.K()
+
+M = my_system.M()
