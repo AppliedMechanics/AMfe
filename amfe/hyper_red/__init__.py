815d75dc6c92a18c4e4fa0a5d04171cca5d1c52f
Johannes Rutzmoser <johannes.rutzmoser@tum.de>
2017-07-14 10:06:51 +0200

    Fixed import causing crash of hyper_red module

--- amfe/hyper_red/__init__.py
+++ amfe/hyper_red/__init__.py
@@ -2,8 +2,7 @@
 Hyper reduction module
 '''
 
-from .poly_reduction import *
-from .poly_system import *
+# from .poly_reduction import *
+# from .poly_system import *
 from .ecsw import *
 from .deim import *
-
