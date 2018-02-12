3596c77b20db4d28dcbf64b85845b8e52a637d6d
Christopher Lerch (RT) <christopher.lerch@tum.de>
2018-02-12 18:05:09 +0100

    Started signal norms.

--- amfe/linalg/norms.py
+++ amfe/linalg/norms.py
@@ -4,7 +4,7 @@
 # Distributed under BSD-3-Clause License. See LICENSE-File for more information
 #
 """
-Module contains vector, matrix, time signal and system norms
+Module contains vector, matrix, signal and system norms
 """
 
 
@@ -12,5 +12,23 @@ import numpy as np
 
 
 __all__ = [
+    'signal_norm'
 ]
+
+
+def signal_norm(x, t=None, dt=1.0, ord=None):
+    if ord is 1:
+        norm_x = np.trapz(np.abs(x), t, dt)
+    elif (ord is None) or (ord is 2):
+        norm_x = np.sqrt(np.trapz(x**2, t, dt))
+    elif ord is 'inf':
+        norm_x = np.max(np.abs(x))
+    else:
+        try:
+            ord + 1
+        except TypeError:
+            raise ValueError('Error: Invalid norm order for signals.')
+        norm_x = np.trapz(np.abs(x)**ord, t, dt)**(1/ord)
+
+    return norm_x
 
