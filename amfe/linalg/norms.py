e69606399c6bb49f06d26415d58ed9fc96673a36
Christopher Lerch (RT) <christopher.lerch@tum.de>
2018-02-13 12:59:26 +0100

    Added H_2-system norm. Not complete yet. H_infty-system norm also missing.

--- amfe/linalg/norms.py
+++ amfe/linalg/norms.py
@@ -4,15 +4,17 @@
 # Distributed under BSD-3-Clause License. See LICENSE-File for more information
 #
 """
-Module contains vector, matrix, signal and system norms
+Module contains all types of norms
 """
 
 
 import numpy as np
+import scipy as sp
 
 
 __all__ = [
-    'signal_norm'
+    'signal_norm',
+    'lti_system_norm'
 ]
 
 
@@ -59,13 +61,13 @@ def signal_norm(x, t=None, dt=1.0, ord=2, axis=-1):
     ====  ========================================
     """
 
-    if ord is 1:
+    if ord == 1:
         norm_x = np.trapz(y=np.abs(x), x=t, dx=dt, axis=axis)
-    elif ord is 2:
+    elif ord == 2:
         norm_x = np.sqrt(np.trapz(y=x**2, x=t, dx=dt, axis=axis))
-    elif ord is np.inf:
+    elif ord == np.inf:
         norm_x = np.amax(a=np.abs(x), axis=axis)
-    elif ord is -np.inf:
+    elif ord == -np.inf:
         norm_x = np.amin(a=np.abs(x), axis=axis)
     else:
         try:
@@ -76,4 +78,31 @@ def signal_norm(x, t=None, dt=1.0, ord=2, axis=-1):
             raise ValueError('Error: Invalid norm order for signals.')
         norm_x = np.trapz(y=np.abs(x)**(1.0*ord), x=t, dx=dt, axis=axis)**(1/ord)
     return norm_x
+
+
+def lti_system_norm(A, B, C, E=None, ord=2):
+    if ord == 2:
+        if E is not None:
+            A = sp.sparse.linalg.spsolve(E, A)
+            B = sp.sparse.linalg.spsolve(E, B)
+        if len(B.shape) == 1:
+            B = B.reshape((-1, 1))
+        if len(C.shape) == 1:
+            C = C.reshape((1, -1))
+        G_c = sp.linalg.solve_continuous_lyapunov(A.todense(), -B@B.T)
+        norm_sys_c = np.sqrt(np.trace(C@G_c@C.T))
+        G_o = sp.linalg.solve_continuous_lyapunov(A.todense().T, -C.T@C)
+        norm_sys_o = np.sqrt(np.trace(B.T@G_o@B))
+        print(norm_sys_c)
+        print(norm_sys_o)
+        print((norm_sys_c - norm_sys_o)/norm_sys_c)
+        if abs((norm_sys_o - norm_sys_c)/norm_sys_c) < 1.0e-6:
+            norm_sys = norm_sys_c
+        else:
+            raise ValueError('Error: H_2-norm calculation failed.')
+    elif ord == np.inf:
+        raise ValueError('Error: Not implemented yet. You may do so.')
+    else:
+        raise ValueError('Error: Invalid norm order for systems.')
+    return norm_sys
 
