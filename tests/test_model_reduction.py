c5e0e2082f05350d0136495c2305e279b4964aa0
Christopher Lerch rt home <christopher.lerch@tum.de>
2018-02-21 12:18:06 +0100

    Adapted test of principle angles to new extended algorithm.

--- tests/test_model_reduction.py
+++ tests/test_model_reduction.py
@@ -45,17 +45,33 @@ def test_principal_angles():
     A = np.random.rand(n_dim, n_vec)
     B = np.random.rand(n_dim, n_vec)
 
-    gamma, F1, F2 = principal_angles(V1=A, V2=B, unit='deg', method=None, principal_vectors=True)
-
+    gamma, F1, F2 = principal_angles(V1=A, V2=B, unit=None, method='sin', principal_vectors=True)
     # test orthogonality of F1
-    np.testing.assert_almost_equal(F1[:,:n_overlap].T @ F1[:,:n_overlap],
-                                   np.eye(n_overlap))
+    np.testing.assert_almost_equal(F1[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
     # test orthogonality of F2
-    np.testing.assert_almost_equal(F2[:,:n_overlap].T @ F2[:,:n_overlap],
-                                   np.eye(n_overlap))
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F2[:,:n_overlap], np.eye(n_overlap))
     # test equality of F1 and F2 in the intersecting subspace
-    np.testing.assert_almost_equal(F2[:,:n_overlap].T @ F1[:,:n_overlap],
-                                   np.eye(n_overlap))
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
+    # test principal angle sines of intersecting subspace
+    np.testing.assert_almost_equal(gamma[:n_overlap], np.zeros(n_overlap))
+
+    gamma, F1, F2 = principal_angles(V1=A, V2=B, unit=None, method='cos', principal_vectors=True)
+    # test orthogonality of F1
+    np.testing.assert_almost_equal(F1[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
+    # test orthogonality of F2
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F2[:,:n_overlap], np.eye(n_overlap))
+    # test equality of F1 and F2 in the intersecting subspace
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
     # test principal angle cosines of intersecting subspace
     np.testing.assert_almost_equal(gamma[:n_overlap], np.ones(n_overlap))
+
+    gamma, F1, F2 = principal_angles(V1=A, V2=B, unit='deg', method=None, principal_vectors=True)
+    # test orthogonality of F1
+    np.testing.assert_almost_equal(F1[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
+    # test orthogonality of F2
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F2[:,:n_overlap], np.eye(n_overlap))
+    # test equality of F1 and F2 in the intersecting subspace
+    np.testing.assert_almost_equal(F2[:,:n_overlap].T@F1[:,:n_overlap], np.eye(n_overlap))
+    # test principal angles of intersecting subspace
+    np.testing.assert_almost_equal(gamma[:n_overlap], np.zeros(n_overlap))
 
