061bf164a0a4b1f02122e36a4a1762c5f68622c4
Christian Meyer <christian.meyer@tum.de>
2018-09-06 11:40:14 +0200

    Added get_nodeids_by_nodeidxs to mesh and added amfemeshobj reader

--- amfe/io.py
+++ amfe/io.py
@@ -21,6 +21,9 @@ __all__ = [
     'MeshConverter',
     'GidAsciiMeshReader',
     'GidJsonMeshReader',
+    'GmshAsciiMeshReader',
+    'AmfeMeshObjMeshReader',
+    'AmfeMeshConverter'
 ]
 
 
@@ -426,6 +429,53 @@ class GmshAsciiMeshReader(MeshReader):
         self.builder.build_mesh_dimension(self._dimension)
 
         return self.builder.return_mesh()
+
+
+class AmfeMeshObjMeshReader(MeshReader):
+    """
+    Reader for Amfe mesh objects
+
+    """
+
+    def __init__(self, meshobj, builder):
+        super().__init__()
+        self._builder = builder
+        self._meshobj = meshobj
+
+    def parse(self):
+        nodeid2idx = self._meshobj.nodeid2idx
+        # build dimension
+        self._builder.build_mesh_dimension(self._meshobj.dimension)
+        self._builder.build_no_of_nodes(self._meshobj.no_of_nodes)
+        self._builder.build_no_of_elements(self._meshobj.no_of_elements + self._meshobj.no_of_boundary_elements)
+        # build nodes
+        for nodeid in nodeid2idx:
+            if self._meshobj.dimension == 2:
+                self._builder.build_node(nodeid,
+                                         self._meshobj.nodes[nodeid2idx[nodeid], 0],
+                                         self._meshobj.nodes[nodeid2idx[nodeid], 1],
+                                         0.0)
+            else:
+                self._builder.build_node(nodeid,
+                                         self._meshobj.nodes[nodeid2idx[nodeid], 0],
+                                         self._meshobj.nodes[nodeid2idx[nodeid], 1],
+                                         self._meshobj.nodes[nodeid2idx[nodeid], 2])
+        # build elements
+        elementid2idx = self._meshobj.eleid2idx
+        connectivitylist = (self._meshobj.connectivity, self._meshobj.boundary_connectivity)
+        eleshapeslist = (self._meshobj.ele_shapes, self._meshobj.boundary_ele_shapes)
+        for elementid in elementid2idx:
+            vol_boundary_flag, idx = elementid2idx[elementid]
+            etype = eleshapeslist[vol_boundary_flag][idx]
+            connectivity = connectivitylist[vol_boundary_flag][idx]
+            connectivity = self._meshobj.get_nodeids_by_nodeidxs(connectivity)
+            self._builder.build_element(elementid, etype, connectivity)
+        # build groups
+        for group in self._meshobj.groups:
+            self._builder.build_group(group,
+                                      self._meshobj.groups[group]['nodes'],
+                                      self._meshobj.groups[group]['elements'])
+        return self._builder.return_mesh()
 
 
 class MeshConverter:
