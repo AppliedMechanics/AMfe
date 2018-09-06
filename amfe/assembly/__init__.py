3b24d7b3e7802287e30a2570ac032350e3ddd1cd
Christian Meyer <christian.meyer@tum.de>
2018-09-06 15:31:06 +0200

    Refactor structure in assembly

---
+++ amfe/assembly/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2018 TECHNICAL UNIVERSITY OF MUNICH, DEPARTMENT OF MECHANICAL ENGINEERING, CHAIR OF APPLIED MECHANICS,
+# BOLTZMANNSTRASSE 15, 85748 GARCHING/MUNICH, GERMANY, RIXEN@TUM.DE.
+#
+# Distributed under 3-Clause BSD license. See LICENSE file for more information.
+#
+
+"""
+Assembly module.
+
+Module for assembly algorithms
+"""
+
+# --- ABSTRACT SUPER CLASS ---
+from .assembly import *  # abstract super class
+
+
+# --- STRUCTURAL ASSEMBLY ---
+from .structural_assembly import *
---
+++ amfe/assembly/assembly.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2018 TECHNICAL UNIVERSITY OF MUNICH, DEPARTMENT OF MECHANICAL ENGINEERING, CHAIR OF APPLIED MECHANICS,
+# BOLTZMANNSTRASSE 15, 85748 GARCHING/MUNICH, GERMANY, RIXEN@TUM.DE.
+#
+# Distributed under 3-Clause BSD license. See LICENSE file for more information.
+#
+
+"""
+Module with abstract class for assembly algorithms
+
+"""
+
+__all__ = [
+    'Assembly'
+]
+
+
+class Assembly:
+    pass
