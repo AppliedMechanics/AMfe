38509c89a952699ae7e011e9266200965033336c
Christian Meyer <christian.meyer@tum.de>
2019-01-31 16:57:51 +0100

    added amfe result to xdmf conversion functionality

--- amfe/io/postprocessing/reader/amfe_solution_reader.py
+++ amfe/io/postprocessing/reader/amfe_solution_reader.py
@@ -1,4 +1,12 @@
+#
+# Copyright (c) 2018 TECHNICAL UNIVERSITY OF MUNICH, DEPARTMENT OF MECHANICAL ENGINEERING, CHAIR OF APPLIED MECHANICS,
+# BOLTZMANNSTRASSE 15, 85748 GARCHING/MUNICH, GERMANY, RIXEN@TUM.DE.
+#
+# Distributed under 3-Clause BSD license. See LICENSE file for more information.
+#
+
 import numpy as np
+import logging
 
 from ..base import PostProcessorReader
 from .. import MeshEntityType, PostProcessDataType
@@ -22,6 +30,7 @@ class AmfeSolutionReader(PostProcessorReader):
         self._amfesolution = amfesolution
         self._meshcomponent = meshcomponent
         self._is_constrained = is_constrained
+        self.logger = logging.getLogger('amfe.postprocessing.reader.AmfeSolutionReader')
         return
 
     def parse(self, builder):
@@ -39,15 +48,26 @@ class AmfeSolutionReader(PostProcessorReader):
         # Get full vector: u_full = f(q).... not implemented yet
             raise NotImplementedError('is_constrained option not implemented yet')
         else:
-            raise NotImplementedError('This function is not implemented yet. It can be implemented'
-                                      'when the solvers are finished in refactoring')
-
+            no_of_timesteps = len(self._amfesolution.t)
             t = np.array(self._amfesolution.t)
-            u = np.array(self._amfesolution.q)
+            u_unconstrained = np.array(self._amfesolution.q).T
             if self._amfesolution.dq[0] is not None or self._amfesolution.ddq[0] is not None:
-                raise NotImplementedError('Velocities and Accelerations cannot be written')
-            mapping = self._meshcomponent._mapping.nodal2global.values.reshape(-1)
-            for i, u_current in enumerate(u):
-                u[i, :] = u_current[mapping]
-            index = self._meshcomponent._mapping.nodal2global.index
-            builder.write_field('displacement', PostProcessDataType.VECTOR, t, u, index, MeshEntityType.NODE)
+                self.logger.warning('Velocities and Accelerations cannot be written by AmfeSolutionReader')
+            mapping = self._meshcomponent._mapping
+            no_of_nodes = self._meshcomponent._mesh.no_of_nodes
+            # Allocate empty array:
+            data = np.empty((no_of_nodes, 3, no_of_timesteps))
+            data[:, :, :] = np.nan
+            nodal2global = mapping.nodal2global.values
+            # Write ux
+            nodeids = mapping._nodal2global.index.values
+            nodeidxs = self._meshcomponent._mesh.get_nodeidxs_by_nodeids(nodeids)
+            data[nodeidxs, 0, :] = u_unconstrained[nodal2global[:, 0], :]
+            data[nodeidxs, 1, :] = u_unconstrained[nodal2global[:, 1], :]
+            if nodal2global.shape[1] > 2:
+                data[nodeidxs, 2, :] = u_unconstrained[nodal2global[:, 2], :]
+            else:
+                data[nodeidxs, 2, :] = np.zeros((len(nodeidxs), no_of_timesteps))
+
+            index = self._meshcomponent._mesh.nodes_df.index.values
+            builder.write_field('displacement', PostProcessDataType.VECTOR, t, data, index, MeshEntityType.NODE)
