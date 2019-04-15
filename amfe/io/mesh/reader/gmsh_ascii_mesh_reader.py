5c711c5126b4fd2f399f19793c91ec8aaab1e280
Christian Meyer <christian.meyer@tum.de>
2019-04-15 11:22:51 +0200

    Fixed bug in gmsh reader and postprocessing converter for hex20 elements

--- amfe/io/mesh/reader/gmsh_ascii_mesh_reader.py
+++ amfe/io/mesh/reader/gmsh_ascii_mesh_reader.py
@@ -9,6 +9,8 @@
 Gmsh ascii mesh reader for I/O module.
 """
 
+import numpy as np
+
 from amfe.io.mesh.base import MeshReader
 from collections.abc import Iterable
 
@@ -27,7 +29,7 @@ class GmshAsciiMeshReader(MeshReader):
         2: 'Tri3',
         3: 'Quad4',
         4: 'Tet4',
-        5: 'Hex8',
+        5: 'Hexa8',
         6: 'Prism6',
         7: None,  # Pyramid
         8: 'quadratic_line',
@@ -39,7 +41,7 @@ class GmshAsciiMeshReader(MeshReader):
         14: None,  # 2nd order pyramid
         15: 'point',
         16: 'Quad8',
-        17: 'Hex20',
+        17: 'Hexa20',
         18: None,  # 15node 2nd order prism
         19: None,  # 13 node pyramid
         20: None,  # 9 node triangle
@@ -72,8 +74,6 @@ class GmshAsciiMeshReader(MeshReader):
     def __init__(self, filename=None):
         super().__init__()
         self._filename = filename
-        # Default dimension is 2, later (during build of elements) it is checked if mesh is 3D
-        self._dimension = 2
         return
 
     def parse(self, builder):
@@ -244,8 +244,6 @@ class GmshAsciiMeshReader(MeshReader):
 
         builder.build_tag(tags_dict)
 
-        builder.build_mesh_dimension(self._dimension)
-
         return
 
 
@@ -269,7 +267,18 @@ class ListElement:
 
         self.no_of_tags = int(elementinfo[2])
         self.connectivity = elementinfo[2 + self.no_of_tags + 1:]
-        self.connectivity = [int(node) for node in self.connectivity]
+        self.connectivity = np.array([int(node) for node in self.connectivity])
+
+        # Change the indices of Tet10-elements, as they are numbered differently
+        # from the numbers used in AMFE and ParaView (last two indices permuted)
+        if self.type == 'Tet10':
+            self.connectivity[np.array([9, 8], dtype=int)] = \
+                self.connectivity[np.array([8, 9], dtype=int)]
+        # Same node numbering issue with Hexa20
+        if self.type == 'Hexa20':
+            hexa8_gmsh_swap = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 11, 13, 9, 16, 18, 19,
+                                        17, 10, 12, 14, 15], dtype=int)
+            self.connectivity[:] = self.connectivity[hexa8_gmsh_swap]
 
         self.physical_group = int(elementinfo[3])
         self.tag = int(elementinfo[4])
