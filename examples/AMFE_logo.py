00cf332110e50fab39c6bf67ff0f8a0fea11db3e
Christian Meyer <christian.meyer@tum.de>
2019-08-19 15:50:18 +0200

    Fixed amfe logo example

--- examples/AMFE_logo.py
+++ examples/AMFE_logo.py
@@ -12,57 +12,82 @@ Created on Fri Dec 18 15:31:45 2015
 
 
 import numpy as np
-import scipy as sp
-import amfe
+from amfe.io import amfe_dir
+from amfe.ui import *
+from amfe.material import KirchhoffMaterial
+from amfe.component import StructuralComponent
+from amfe.solver import SolverFactory, AmfeSolution
+from amfe.solver.translators import create_constrained_mechanical_system_from_component
+from amfe.structural_dynamics import vibration_modes
+
+meshfile = amfe_dir('meshes/gmsh/AMFE_logo.msh')
+output_file = amfe_dir('results/AMFE_logo/logo_5_v2.xdmf')
 
 
-input_file = amfe.amfe_dir('meshes/gmsh/AMFE_logo.msh')
-output_file = amfe.amfe_dir('results/AMFE_logo/logo_5')
+material_1 = KirchhoffMaterial(E=5E6, rho=1E4)
+material_2 = KirchhoffMaterial(E=5E7, rho=1E4)
+
+mesh = import_mesh_from_file(meshfile)
+
+my_component = StructuralComponent(mesh)
+my_component.assign_material(material_1, [299, 300], 'S')
+my_component.assign_material(material_2, [301, 302], 'S')
+
+set_dirichlet_by_group(my_component, 298, ('ux', 'uy', 'uz'))
+
+no_of_dofs = my_component.mapping.no_of_dofs
+q0 = np.zeros(no_of_dofs)
+nodeids = my_component.mesh.nodes_df.index.to_list()
+dofnumbers = my_component.mapping.get_dofs_by_nodeids(nodeids, ('uy', ))
+M_unconstr = my_component.M(q0, q0, 0.0)
+
+g = np.zeros_like(q0)
+for dofnumber in dofnumbers:
+    g[dofnumber] = -9.81
 
 
-material_1 = amfe.KirchhoffMaterial(E=5E6, rho=1E4)
-material_2 = amfe.KirchhoffMaterial(E=5E7, rho=1E4)
-my_system = amfe.MechanicalSystem()
-
-
-my_system.load_mesh_from_gmsh(input_file, 299, material_1)
-my_system.load_mesh_from_gmsh(input_file, 300, material_1)
-my_system.load_mesh_from_gmsh(input_file, 301, material_2)
-my_system.load_mesh_from_gmsh(input_file, 302, material_2)
-
-
-my_system.apply_dirichlet_boundaries(298, 'xyz')
-M_unconstr = my_system.assembly_class.assemble_m()
-no_of_nodes = my_system.mesh_class.no_of_nodes
-#g = [(0, -9.81, 0) for i in range(no_of_nodes)]
-g = [(0, -9.81, 0) for i in range(no_of_nodes)]
-g = np.array(g).flatten()
 f_gravity = M_unconstr @ g
 
-def f_ext_unconstr(u=None, t=None):
-#    if t > 0.5:
-#        return f_gravity*0
-#    else:
-        return f_gravity*np.sin(t*2*np.pi*4.2)
 
-my_system._f_ext_unconstr = f_ext_unconstr
-
-om, V = amfe.vibration_modes(my_system, 40)
-
-print('The vibration modes eigenfrequencies (1/s) are:\n', om/(2*np.pi))
-
-#amfe.solve_linear_displacement(my_system)
-#amfe.solve_nonlinear_displacement(my_system, no_of_load_steps=50)
-
-# time integration
-dt = 1E-2
-T = np.arange(0,4,dt)
-ndof = my_system.dirichlet_class.no_of_constrained_dofs
-q0 = dq0 = np.zeros(ndof)
-solver = amfe.NonlinearDynamicsSolver(my_system, initial_conditions={'q0': q0, 'dq0': dq0}, verbose=True, t_end=4, dt=dt)
-solver.solve()
-# old api: amfe.integrate_nonlinear_system(my_system, q0, dq0, T, dt, alpha=0.01)
+def my_fext(u, du, t):
+    return f_gravity*t
 
 
-my_system.export_paraview(output_file)
+my_component.f_ext = my_fext
 
+my_system, my_formulation = create_constrained_mechanical_system_from_component(my_component, True, True)
+
+x0 = np.zeros(my_system.dimension)
+
+om, V = vibration_modes(my_system.K(x0, x0, 0.0), my_system.M(x0, x0, 0.0), 10)
+
+print('The vibration modes eigenfrequencies (Hz) are:\n', om/(2*np.pi))
+
+solfac = SolverFactory()
+
+solfac.set_system(my_system)
+solfac.set_analysis_type('static')
+solfac.set_dt_initial(0.01)
+solfac.set_analysis_type('zero')
+solfac.set_linear_solver('pardiso')
+solfac.set_newton_verbose(True)
+solfac.set_nonlinear_solver('newton')
+solfac.set_newton_atol(1e-5)
+solfac.set_newton_rtol(1e-7)
+solver = solfac.create_solver()
+
+solution = AmfeSolution()
+
+
+def write_callback(t, x, dx, ddx):
+    u, du, ddu = my_formulation.recover(x, dx, ddx, t)
+    solution.write_timestep(t, u, du, ddu)
+
+
+dx0 = x0.copy()
+t0 = 0.0
+t_end = 1.0
+
+solver.solve(write_callback, t0, x0, dx0, t_end)
+
+write_results_to_paraview(solution, my_component, output_file)
